8e6f7a3ee3cdb23e9f1889795a53ceb9af2fc0c8
harshith <svs.harshith@gmail.com>
2020-03-10 00:30:28 +0530

    single first comm

---
+++ app.js
@@ -0,0 +1,54 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+
+mongoose.connect('mongodb://localhost/urlShortener', {
+  useNewUrlParser: true, useUnifiedTopology: true
+})
+
+const shortId = require('shortid')
+
+const shortUrlSchema = new mongoose.Schema({
+  full: {
+    type: String,
+    required: true
+  },
+  short: {
+    type: String,
+    required: true,
+    default: shortId.generate
+  },
+  clicks: {
+    type: Number,
+    required: true,
+    default: 0
+  }
+})
+
+var ShortUrl = mongoose.model("shortUrl", shortUrlSchema);
+
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
+
+app.get('/', async (req, res) => {
+  const shortUrls = await ShortUrl.find()
+  res.render('index', { shortUrls: shortUrls })
+})
+
+app.post('/shortUrls', async (req, res) => {
+  await ShortUrl.create({ full: req.body.fullUrl })
+
+  res.redirect('/')
+})
+
+app.get('/:shortUrl', async (req, res) => {
+  const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl })
+  if (shortUrl == null) return res.sendStatus(404)
+  shortUrl.save()
+
+  res.redirect(shortUrl.full)
+})
+var port=5000;
+app.listen(port, () => {
+  console.log(`doof says yes on ${port}`);
+});
